d0e20729821503f7b55953d226962ae6848260dd
shubham kasalkar <shubhkasal@gmail.com>
2023-12-26 19:33:03 +0530

    Fully running code

--- dataExtraction.py
+++ dataExtraction.py
@@ -3,13 +3,13 @@ import json
 from decimal import Decimal
 from botocore.exceptions import ClientError
 
-def batch_write_with_backoff(client, request_params):
+def batch_write_with_backoff(dynamodb_client, request_params):
     max_retries = 5
     base_delay = 0.1  # Initial delay in seconds
 
     for i in range(max_retries):
         try:
-            response = client.batch_write_item(**request_params)
+            response = dynamodb_client.batch_write_item(**request_params)
             return response
         except ClientError as e:
             if e.response['Error']['Code'] == 'ProvisionedThroughputExceededException':
@@ -19,6 +19,9 @@ def batch_write_with_backoff(client, request_params):
                 raise  # Propagate other errors
 
     raise Exception("Max retries reached. Unable to complete BatchWriteItem operation.")
+
+
+
 def convert_to_decimal(obj):
     if isinstance(obj, float):
         return Decimal(str(obj))
@@ -27,48 +30,6 @@ def convert_to_decimal(obj):
     elif isinstance(obj, list):
         return [convert_to_decimal(item) for item in obj]
     return obj
-
-
-def create_dynamodb_table(dynamodb_table_name,aws_access_key_id,aws_secret_access_key,aws_region):
-    # Specify your AWS region
-    aws_region = aws_region
-
-    # Specify your DynamoDB table name
-    table_name = dynamodb_table_name
-
-    # Specify the primary key attribute (partition key)
-    partition_key_name = 'DocumentId'  # Replace with your desired partition key attribute
-
-    # Specify the attribute types
-    partition_key_type = 'S'  # 'S' for string, 'N' for number, etc.
-
-    # Create a DynamoDB client
-    dynamodb = boto3.client('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,region_name=aws_region)
-
-    # Define the KeySchema, AttributeDefinitions, and ProvisionedThroughput
-    key_schema = [
-        {'AttributeName': partition_key_name, 'KeyType': 'HASH'}  # Partition Key
-    ]
-
-    attribute_definitions = [
-        {'AttributeName': partition_key_name, 'AttributeType': partition_key_type}
-    ]
-
-    provisioned_throughput = {
-        'ReadCapacityUnits': 5,   # Adjust based on your read requirements
-        'WriteCapacityUnits': 5   # Adjust based on your write requirements
-    }
-
-    # Create the DynamoDB table
-    response = dynamodb.create_table(
-        TableName=table_name,
-        KeySchema=key_schema,
-        AttributeDefinitions=attribute_definitions,
-        ProvisionedThroughput=provisioned_throughput
-    )
-
-    print("Table creation response:", response)
-
 
 
 def get_json_data_from_s3(bucket_name,prefix,aws_access_key_id,aws_secret_access_key,aws_region):
@@ -97,55 +58,43 @@ def get_json_data_from_s3(bucket_name,prefix,aws_access_key_id,aws_secret_access
     return json_data_dict
 
 
-def load_data_into_dynamodb(data_dict, table_name, aws_access_key_id,aws_secret_access_key,aws_region):
-    # Create a DynamoDB resource
-    dynamodb = boto3.resource('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,region_name=aws_region)
+def does_table_exist(table_name, dynamodb_client):
+    try:
+        dynamodb_client.describe_table(TableName=table_name)
+        return True
+    except dynamodb_client.exceptions.ResourceNotFoundException:
+        return False
 
-    # Specify your DynamoDB table name
-    table_name = table_name
 
-    # Specify the primary key attribute (partition key)
-    partition_key_name = 'DocumentId'  # Replace with your desired partition key attribute
-
-    # Specify the attribute types
-    partition_key_type = 'S'  # 'S' for string, 'N' for number, etc.
-
-    # Define the KeySchema, AttributeDefinitions, and ProvisionedThroughput
-    key_schema = [
-        {'AttributeName': partition_key_name, 'KeyType': 'HASH'}  # Partition Key
-    ]
-
-    attribute_definitions = [
-        {'AttributeName': partition_key_name, 'AttributeType': partition_key_type}
-    ]
-
-    provisioned_throughput = {
-        'ReadCapacityUnits': 5,   # Adjust based on your read requirements
-        'WriteCapacityUnits': 5   # Adjust based on your write requirements
-    }
-
-    # Create the DynamoDB table
-    response = dynamodb.create_table(
+def create_dynamodb_table(table_name, partition_key_name, partition_key_type, dynamodb_client):
+    response = dynamodb_client.create_table(
         TableName=table_name,
-        KeySchema=key_schema,
-        AttributeDefinitions=attribute_definitions,
-        ProvisionedThroughput=provisioned_throughput
+        KeySchema=[
+            {'AttributeName': partition_key_name, 'KeyType': 'HASH'}  # Partition key
+        ],
+        AttributeDefinitions=[
+            {'AttributeName': partition_key_name, 'AttributeType': partition_key_type}
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
     )
+    print("Table created:", response)
 
-    print("Table creation response:", response)
 
-    # Specify the DynamoDB table
-    #table = dynamodb.Table(table_name)
+def insert_data_into_dynamodb(table_name, data, dynamodb_client,aws_region,aws_access_key_id,aws_secret_access_key):
+    table = boto3.resource('dynamodb',region_name=aws_region,aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key).Table(table_name)
 
-    # Iterate through items in the data dictionary and put them into DynamoDB
-    #with table.batch_writer() as batch:
-    with response.batch_writer() as batch:
-        for file_key, json_data in data_dict.items():
+    with table.batch_writer() as batch:
+        for file_key, json_data in data.items():
             # Convert all numeric values to Decimal
             json_data_decimal = convert_to_decimal(json_data)
             batch.put_item(Item=json_data_decimal)
 
-    print(f"Data loaded into DynamoDB table {table_name}.")
+    print("Data inserted into DynamoDB table.")
+
+
 
 
 if __name__ == '__main__':
@@ -162,9 +111,16 @@ if __name__ == '__main__':
     result_dict = get_json_data_from_s3(bucket_name,prefix,aws_access_key_id,aws_secret_access_key,aws_region)
     #print(result_dict)
 
-    dynamodb_table_name = 'dataextraction'
+    table_name = 'dataextraction'
+    partition_key_name = 'cik'
+    partition_key_type = 'N'  # String, change to 'N' for Number
 
-    #create_dynamodb_table(dynamodb_table_name,aws_access_key_id,aws_secret_access_key,aws_region)
+    dynamodb_client = boto3.client('dynamodb', region_name=aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
-    # Load s3 json file data into Dynamodb
-    load_data_into_dynamodb(result_dict, dynamodb_table_name, aws_access_key_id,aws_secret_access_key,aws_region)
+    # Check if the table exists
+    if does_table_exist(table_name, dynamodb_client):
+        print("Table already exists.")
+        insert_data_into_dynamodb(table_name, result_dict, dynamodb_client,aws_region,aws_access_key_id,aws_secret_access_key)
+    else:
+        # Create the table if it doesn't exist
+        create_dynamodb_table(table_name, partition_key_name, partition_key_type, dynamodb_client)        
